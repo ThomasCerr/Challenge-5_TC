a91d78f0a8ef4b28066e09da743a28e19e51533d
ThomasCerr <99366289+ThomasCerr@users.noreply.github.com>
2022-04-03 15:30:27 -0400

    time blocks complete

---
+++ README.md
@@ -0,0 +1,40 @@
+# Challenge-5
+
+```
+A portfolio of work can showcase your skills and talents to employers looking to fill a part-time or full-time position.
+An effective portfolio highlights your strongest work as well as the thought processes behind it.
+Students who have portfolios with deployed web applications (meaning they are live on the web) are typically very
+successful in their career search after the boot camp. This last point can’t be stressed enough: having several 
+deployed projects is a minimum requirement to receive an initial interview at many companies.
+```
+
+
+## User Story
+```
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
+```
+
+
+## Acceptance Criteria
+```
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with time blocks for standard business hours
+WHEN I view the time blocks for that day
+THEN each time block is color-coded to indicate whether it is in the past, present, or future
+WHEN I click into a time block
+THEN I can enter an event
+WHEN I click the save button for that time block
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+```
+## Mockup
+
+<img src= "https://github.com/ThomasCerr/Challenge-4_TC/blob/master/assets/images/example.PNG">
+
+Link to deployed application: https://thomascerr.github.io/Challenge-5_TC/
---
+++ assets/script.js
@@ -0,0 +1,80 @@
+//date and time at the top
+var tasks;
+
+
+
+var currentHour = moment().hour()
+var time = moment().format("[Today's date is] dddd, MMMM Do")
+var timeDisplay = $("#currentDay");
+timeDisplay.text(time);
+
+var textArea = $(".color"); //reference to each timeblock
+
+
+var timeColor = function() { 
+    for (var i= 0; i < textArea.length; i++ ){
+        var hour = parseInt($(textArea[i]).attr("id"));
+        console.log(hour)
+    
+
+        if (hour < currentHour){
+            $(textArea[i]).addClass("past");
+        } else if (hour == currentHour){
+            $(textArea[i]).addClass("present");
+        } else  {
+            $(textArea[i]).addClass("future");
+        }}}
+
+
+
+var saveTasks = function(){
+localStorage.setItem("tasks",JSON.stringify(tasks));
+
+
+
+
+
+$('.saveBtn').each(function(event) {
+ $(this).on("click", function(){
+var text = $(this).val().trim();
+var taskArea = $()
+}
+
+//save local storage
+
+        // jquery to all of save button, based on a save button, it will save to local storage, based on time to match the block?
+                
+
+//save local storage
+
+        // jquery to all of save button, based on a save button, it will save to local storage, based on time to match the block?
+        // array that will = a Json
+
+//load local storage
+        //pull the json above
+            //find text area based on id, set it to whatever is saved in local storage
+
+// function loadLocalStorage () {}
+
+// //click listener to save buttons
+// $('.saveBtn').each(function(event) {
+//     $(this).on("click", function(){
+        
+
+
+
+// // local storage Json
+// var savedTask = JSON.parse(localStorage.getItem('savedTask'));
+// if (savedTask == null) {
+//     savedTask = [];
+// }
+// // saves text to array
+// plans[saveHour] = toSaveText;
+// localStorage.setItem('plans', JSON.stringify(plans));
+            
+
+
+//     })
+// })
+
+timeColor();
